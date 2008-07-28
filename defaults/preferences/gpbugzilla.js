72607e08b1fedcf30a79fdf99f374473f79f6d17
Andrew Sutherland <asutherland@asutherland.org>
2008-07-27 22:40:23 -0700

    initial import from mielczarek's magic extension factory

---
+++ defaults/preferences/gpbugzilla.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.gpbugzilla@mozilla.messaging.inc..description", "chrome://gpbugzilla/locale/gpbugzilla.properties");
